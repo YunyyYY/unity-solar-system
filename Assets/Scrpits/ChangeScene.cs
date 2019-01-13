b01e0f9a938ecbf90f006dbaf9d6d7d6b6e81e41
yunyy <lyguokelly@gmail.com>
2019-01-13 12:29:42 +0800

    First Commit

---
+++ Assets/Scrpits/ChangeScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour {
+
+    public Button mButton;
+    public string sceneName;
+
+    // Use this for initialization
+    void Start () {
+        //Gets ButtonMount
+        Button btnMount = mButton.GetComponent<Button>();
+        //add a listener to ButtonMount, executing TaskOnClick() when click ButtonMount
+        btnMount.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Loading Scene1
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string sceneName;
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    // Update is called once per frame
+    void Click () {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+*/
---
+++ Assets/Scrpits/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Transform Origin;    //各天体公转的圆心
+	public float Gspeed;        //公转速度
+	public float Zspeed;        //自转速度
+	public float Ry, Rz;        //通过y轴、z轴调整公转的偏心率，使其不在同一平面公转
+
+	void Start() {
+
+	}
+
+	void FixedUpdate () {
+		Vector3 axis = new Vector3 (0, Ry, Rz);     //公转轴
+		this.transform.RotateAround (Origin.position, axis, Gspeed * Time.deltaTime);   //公转
+		this.transform.Rotate (Vector3.up * Zspeed * Time.deltaTime);       //自转
+	}  
+}
